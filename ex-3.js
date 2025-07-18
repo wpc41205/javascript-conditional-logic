4162f2d37b7867d058856293198fb30f9a81c9ae
Bambam <bam.ksnru@gmail.com>
2025-07-18 14:00:32 +0700

    EX:Conditional Logic Assignment

--- ex-3.js
+++ ex-3.js
@@ -3,3 +3,8 @@
 let lightBulbStatus = "Off";
 
 //Start coding here
+let message = (lightBulbStatus === "On") ? "Light bulb is On." :
+              (lightBulbStatus === "Off") ? "Light bulb is Off." :
+              (lightBulbStatus === "Broken") ? "Light bulb is Broken." :
+              "Please choose the correct input (On/Off/Broken)";
+console.log(message);
